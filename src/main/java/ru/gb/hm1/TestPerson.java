ade9ed2785af0f399c317c54d12c68d961150850
DeN <den.denis.denis@mail.ru>
2023-04-16 13:41:16 +0300

    1

---
+++ src/main/java/ru/gb/hm1/TestPerson.java
@@ -0,0 +1,26 @@
+package ru.gb.hm1;
+
+
+import lombok.extern.java.Log;
+
+@Log
+public class TestPerson {
+    public static void main(String[] args) {
+        Person person = Person.createBuilder()
+                .withFirstName("Alex")
+                .withLastName("Juice")
+                .withMiddleName("Banana")
+                .withAddress("Problem Street")
+                .withGender("F")
+                .withAge(19)
+                .withCountry("Latvia")
+                .withPhone("3424242424")
+                .build();
+
+
+     log.info(person.toString());
+
+
+
+    }
+}
